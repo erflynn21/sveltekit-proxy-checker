28762ff8996e70b6894e30728c4dd530e5ca1239
Evan Flynn <erflynn21@gmail.com>
2021-08-03 16:32:51 +0800

    adds auth store and auth listener

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,2 +1,31 @@
-<script>import "../app.postcss";</script>
-<slot></slot>
+<script lang="ts">
+	import '../app.postcss';
+	import firebase from 'firebase/app';
+	import 'firebase/auth';
+	import { onMount } from 'svelte';
+	import authStore from '../stores/authStore';
+
+	onMount(() => {
+		const firebaseConfig = {
+			apiKey: 'AIzaSyDPfzdTZf4QOdPOIeD4XbRW3xXkomml5AA',
+			authDomain: 'proxyrack-proxy-checker.firebaseapp.com',
+			projectId: 'proxyrack-proxy-checker',
+			storageBucket: 'proxyrack-proxy-checker.appspot.com',
+			messagingSenderId: '153719007137',
+			appId: '1:153719007137:web:8834d222e5fc80d280cde0',
+			measurementId: 'G-GCF7CHJ6X7'
+		};
+
+		firebase.initializeApp(firebaseConfig);
+
+		firebase.auth().onAuthStateChanged((user) => {
+			authStore.set({
+				isLoggedIn: user !== null,
+				user,
+				firebaseControlled: true
+			});
+		});
+	});
+</script>
+
+<slot />
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,15 @@
-<h1 class="">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { goto } from '$app/navigation';
+
+	import authStore from '../stores/authStore';
+
+	authStore.subscribe(async ({ isLoggedIn, firebaseControlled }) => {
+		if (!isLoggedIn && firebaseControlled) {
+			await goto('/login');
+		}
+	});
+</script>
+
+{#if $authStore.isLoggedIn}
+	{$authStore.user.email}
+{/if}
