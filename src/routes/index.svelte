bfeab53162f5f8ab62ec80671f6a3783f9c3262f
Evan Flynn <erflynn21@gmail.com>
2021-08-09 16:12:34 +0800

    adds further action options

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,6 +3,14 @@
 	import Nav from '../components/nav.svelte';
 	import userStore from '../stores/userStore';
 
+	let action: string = 'Check Number of IPs in a Country';
+	let product: string = 'Unmetered Residential';
+	let port: string = 'Random';
+	let portNumber: number = 10000;
+	let auth: string = 'Use My Authentication Details';
+	let username: string = '';
+	let password: string = '';
+
 	userStore.subscribe(async ({ isLoggedIn, firebaseControlled }) => {
 		if (!isLoggedIn && firebaseControlled) {
 			await goto('/login');
@@ -15,7 +23,6 @@
 
 	<main class="-mt-32">
 		<div class="max-w-7xl mx-auto pb-12 px-4 sm:px-6 lg:px-8 grid grid-cols-3 mt-8 gap-4">
-			<!-- Replace with your content -->
 			<div class="bg-white rounded-lg shadow-md px-5 py-6 sm:px-6 ">
 				<div class="rounded-lg h-auto">
 					<div>
@@ -23,6 +30,7 @@
 						<select
 							id="action"
 							name="action"
+							bind:value={action}
 							class="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
 						>
 							<option selected>Check Number of IPs in a Country</option>
@@ -40,6 +48,7 @@
 						<select
 							id="product"
 							name="product"
+							bind:value={product}
 							class="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
 						>
 							<option selected>Unmetered Residential</option>
@@ -56,31 +65,86 @@
 						<select
 							id="port"
 							name="port"
+							bind:value={port}
 							class="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
 						>
 							<option selected>Random</option>
 							<option>Sticky</option>
 						</select>
 					</div>
+					{#if port === 'Sticky'}
+						<div
+							class="relative border mt-4 border-gray-300 rounded-md px-3 py-2 shadow-sm focus-within:ring-1 focus-within:ring-indigo-600 focus-within:border-indigo-600"
+						>
+							<label
+								for="portNumber"
+								class="absolute -top-2 left-2 -mt-px inline-block px-1 bg-white text-xs font-medium text-gray-900"
+								>Port Number:</label
+							>
+							<input
+								type="number"
+								name="portNumber"
+								id="portNumber"
+								class="block w-full border-0 p-0 text-gray-900 placeholder-gray-500 focus:ring-0 sm:text-sm"
+								bind:value={portNumber}
+							/>
+						</div>
+					{/if}
 					<div class="mt-4">
 						<label for="auth" class="block text-sm font-medium text-gray-700">Authentication:</label
 						>
 						<select
 							id="auth"
 							name="auth"
+							bind:value={auth}
 							class="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
 						>
 							<option selected>Use My Authentication Details</option>
 							<option>Use a User's Authentication Details</option>
 						</select>
 					</div>
+					{#if auth === `Use a User's Authentication Details`}
+						<div
+							class="relative border mt-4 border-gray-300 rounded-md px-3 py-2 shadow-sm focus-within:ring-1 focus-within:ring-indigo-600 focus-within:border-indigo-600"
+						>
+							<label
+								for="name"
+								class="absolute -top-2 left-2 -mt-px inline-block px-1 bg-white text-xs font-medium text-gray-900"
+								>User's Username</label
+							>
+							<input
+								type="text"
+								name="name"
+								id="name"
+								class="block w-full border-0 p-0 text-gray-900 placeholder-gray-500 focus:ring-0 sm:text-sm"
+								placeholder="Username"
+								bind:value={username}
+							/>
+						</div>
+						<div
+							class="relative border mt-4 border-gray-300 rounded-md px-3 py-2 shadow-sm focus-within:ring-1 focus-within:ring-indigo-600 focus-within:border-indigo-600"
+						>
+							<label
+								for="name"
+								class="absolute -top-2 left-2 -mt-px inline-block px-1 bg-white text-xs font-medium text-gray-900"
+								>User's Password (API Key)</label
+							>
+							<input
+								type="text"
+								name="name"
+								id="name"
+								class="block w-full border-0 p-0 text-gray-900 placeholder-gray-500 focus:ring-0 sm:text-sm"
+								placeholder="Username"
+								bind:value={password}
+							/>
+						</div>
+					{/if}
 				</div>
 				<button
 					type="button"
 					class="inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 mt-4"
 				>
 					Run It!
-					<!-- Heroicon name: solid/mail -->
 					<svg
 						xmlns="http://www.w3.org/2000/svg"
 						class="h-6 w-6"
@@ -117,7 +181,6 @@
 					/>
 				</div>
 			</div>
-			<!-- /End replace -->
 		</div>
 	</main>
 </div>
