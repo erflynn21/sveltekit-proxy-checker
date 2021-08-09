35fb58f9e28187574886226f07f8a50338e1c91b
Evan Flynn <erflynn21@gmail.com>
2021-08-09 16:55:44 +0800

    removes build from git

--- build/_app/chunks/authStore-e1e5500f.js
+++
@@ -1,1 +0,0 @@
-import{C as s}from"./vendor-857c37bd.js";const e=s({isLoggedIn:!1,firebaseControlled:!1});var r={subscribe:e.subscribe,set:e.set};const b=s({username:null,password:null});var o={subscribe:b.subscribe,set:b.set};export{o as a,r as u};
